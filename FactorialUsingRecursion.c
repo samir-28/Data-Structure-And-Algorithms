6f20536ce81e98a3da8a552ab73502141cf0885b
Sawmeer Bazgain <samirbajgain9@gmail.com>
2024-02-01 15:06:15 +0545

    Factorial

---
+++ FactorialUsingRecursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int facto(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return facto(n - 1) * n;
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number to find the factorial :\n");
+    scanf("%d",&n);
+    printf("The factorial of %d is:%d", n, facto(n));
+}
